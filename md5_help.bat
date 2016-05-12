3b7eaa821bfc6b8844881c1e870f92dcba059111
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2016-05-12 06:20:53 -0400

    Add scripts

---
+++ md5_help.bat
@@ -0,0 +1,14 @@
+@echo off
+chcp 1252
+
+
+cls
+
+:: On affiche l'aide à propos du logiciel.
+bin\md5.exe --help
+
+:: On écrit une ligne blanche.
+echo.
+
+:: On ouvre une ligne de commande.
+cmd
---
+++ md5_string.bat
@@ -0,0 +1,17 @@
+@echo off
+chcp 1252
+
+
+cls
+
+:: On demande à l'utilisateur de rentrer un string.
+set /p MESSAGE="Veuillez entrer le message à hacher : "
+
+:: On execute le programme en mode string.
+bin\md5.exe --string "%MESSAGE%"
+
+:: On écrit une ligne blanche.
+echo.
+
+:: On attend l'intervention de l'utilisateur pour quitter.
+pause
