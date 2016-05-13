19b63937cf158d4aaf5d7204940dea618d574423
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2016-05-12 20:40:29 -0400

    Update md5_fichiers.bat

--- md5_fichiers.bat
+++ md5_fichiers.bat
@@ -1,29 +1,22 @@
 @echo off
 
-
-:: On cherche tous les fichiers avec les extensions *.c, et *.h ou qui commencent avec makefile.
+:: On cherche tous les fichiers avec les extensions *.c, et *.h ou qui commencent avec makefile.
 for /f %%A in ('dir *.c *.h makefile* /b/s') do (
-
-    :: On écrit le nom du fichier.
+    :: On Ã©crit le nom du fichier.
     echo %%A
 
-    
     :: On execute le programme en mode fichier.
     bin\md5.exe --file %%A
-
 
     :: On execute le programme en mode stdin en recevant le message par une redirection (pipe).
     type %%A | bin\md5.exe --stdin
 
-    :: On compare avec une implémentation indépendante de MD5.
+    :: On compare avec une implÃ©mentation indÃ©pendante de MD5.
     bin\md5deep.exe %%A 2> nul
 
-    :: On écrit une ligne blanche.
+    :: On Ã©crit une ligne blanche.
     echo.
-
 )
-
-
 
 :: On attend l'intervention de l'utilisateur pour quitter.
 pause
