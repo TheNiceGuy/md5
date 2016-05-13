83bd01ee79ea2a060f091636b377ca14409c4433
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2016-05-12 20:40:44 -0400

    Update md5_help.bat

--- md5_help.bat
+++ md5_help.bat
@@ -1,13 +1,11 @@
 @echo off
-chcp 1252
-
-
+chcp 1252
 cls
 
-:: On affiche l'aide à propos du logiciel.
+:: On affiche l'aide Ã  propos du logiciel.
 bin\md5.exe --help
 
-:: On écrit une ligne blanche.
+:: On Ã©crit une ligne blanche.
 echo.
 
 :: On ouvre une ligne de commande.
